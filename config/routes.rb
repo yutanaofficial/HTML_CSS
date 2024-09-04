c77678264076bd29c21c703b22a47bd4ca3239c3
yuttana chuay <yutanaofficial@gmail.com>
2024-09-05 00:01:27 +0700

    chore: Add basic HTML structure and CSS styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "pages/home"
+  get "pages/about"
+  get "pages/contact"
+ # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ root "pages#home"
+ get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  #
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
   # root "posts#index"
